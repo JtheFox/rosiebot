1eb736e9dbb0ae0f8ab37c5d3f0c7b10bb4eb30d
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-13 12:48:10 -0400

    Refactor & add delete GuildMember fn

--- db/dbOps.js
+++ db/dbOps.js
@@ -29,55 +29,69 @@ const ensureGuild = async (guildId) => {
   }
 }
 
-module.exports = {
-  updateGuild: async (guildId, settings = {}) => {
-    try {
-      const [guild, created] = await Guild.upsert({
-        guildId: guildId,
-        ...settings
-      });
-      if (!guild) throw new Error('Operation failed');
-      created ? logger.log('Guild added to database') : logger.log('Guild updated in database');
-    } catch (err) {
-      logger.error(err);
-      return err;
-    }
-  },
-  ensureGuildMember: async (guildId, userId) => {
-    try {
-      const guild = await ensureGuild(guildId);
-      const user = await ensureUser(userId);
-      if (!guild || !user) throw new Error('Operation failed');
-      if (!guild.hasUser(user)) await guild.addUser(user, { through: {} });
-    } catch (err) {
-      logger.error(err);
-      return err;
-    }
-  },
-  payoutBet: async (guildId, winners = [], losers = []) => {
-    try {
-      winners.length && GuildMember.increment('betWins', {
-        by: 1,
-        where: {
-          guildId: guildId,
-          userId: {
-            [Op.in]: winners
-          }
-        }
-      });
-      winners.length && GuildMember.increment('betLosses', {
-        by: 1,
-        where: {
-          guildId: guildId,
-          userId: {
-            [Op.in]: losers
-          }
-        }
-      });
-      logger.log('Bet has been paid out');
-    } catch (err) {
-      logger.error(err);
-      return err;
-    }
+const updateGuild = async (guildId, settings = {}) => {
+  try {
+    const [guild, created] = await Guild.upsert({
+      guildId: guildId,
+      ...settings
+    });
+    if (!guild) throw new Error('Operation failed');
+    created ? logger.log('Guild added to database') : logger.log('Guild updated in database');
+  } catch (err) {
+    logger.error(err);
+    return err;
   }
 }
+
+const ensureGuildMember = async (guildId, userId) => {
+  try {
+    const guild = await ensureGuild(guildId);
+    const user = await ensureUser(userId);
+    if (!guild || !user) throw new Error('Operation failed');
+    if (!guild.hasUser(user)) await guild.addUser(user, { through: {} });
+  } catch (err) {
+    logger.error(err);
+    return err;
+  }
+}
+
+const deleteGuildMember = async (guildId, userId) => {
+  try {
+    const guild = await ensureGuild(guildId);
+    const user = await ensureUser(userId);
+    if (!guild || !user) throw new Error('Operation failed');
+    if (guild.hasUser(user)) await guild.removeUser(user, { through: {} });
+  } catch (err) {
+    logger.error(err);
+    return err;
+  }
+}
+
+const payoutBet = async (guildId, winners = [], losers = []) => {
+  try {
+    winners.length && GuildMember.increment('betWins', {
+      by: 1,
+      where: {
+        guildId: guildId,
+        userId: {
+          [Op.in]: winners
+        }
+      }
+    });
+    winners.length && GuildMember.increment('betLosses', {
+      by: 1,
+      where: {
+        guildId: guildId,
+        userId: {
+          [Op.in]: losers
+        }
+      }
+    });
+    logger.log('Bet has been paid out');
+  } catch (err) {
+    logger.error(err);
+    return err;
+  }
+}
+
+module.exports = { ensureUser, ensureGuild, updateGuild, ensureGuildMember, deleteGuildMember, payoutBet }
