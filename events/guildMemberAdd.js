50f7d950e4859cbfa97f7f2bc0d4f127ff340ad1
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-13 12:49:03 -0400

    Replace unused handler with guildMemberRemove handler

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-const logger = require('../utils/logger');
-// On new member joining a server
-module.exports = (client, member) => {
-  // Currently unused
-};
---
+++ events/guildMemberRemove.js
@@ -0,0 +1,11 @@
+const logger = require('../utils/logger');
+const { deleteGuildMember } = require('../db/dbOps');
+// On member leaving a server
+module.exports = (client, member) => {
+  try {
+    await deleteGuildMember(member.guild.id, member.id);
+    logger.warn('GuildMember successfully deleted from database', member.id)
+  } catch (err) {
+    logger.error(['Failed to delete GuildMember', member.id, err]);
+  }
+};
