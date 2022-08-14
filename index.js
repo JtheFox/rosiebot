73fd8a854e73aca8bb2a5f0cf6127a504d7e759c
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-14 11:51:44 -0400

    Re-add slash command handling for betting feature

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { readdir } = require('fs').promises;
 const logger = require('./utils/logger.js');
 const { intents, partials, permLevels } = require('./config.js');
 const sequelize = require('./db/connection.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
 
 // Instantiate client
 const client = new Client({ intents, partials });
@@ -42,27 +44,55 @@ const init = async () => {
     }
   });
 
+  // Load slash commands
+  logger.log(`Loading ${slashFiles.length} commands`);
+  slashFiles.filter(file => file.endsWith('.js')).forEach(slash => {
+    try {
+      logger.log(`Loading Slash Command: ${slash}`);
+      const props = require(`./slash/${slash}`);
+      client.container.slashcmds.set(props.cmd.name, props);
+    } catch (err) {
+      logger.error(`Failed to load slash command ${slash}: ${err}`);
+    }
+  });
+
   // Load events
   logger.log(`Loading ${evtFiles.length} events`);
   evtFiles.filter(file => file.endsWith('.js')).forEach(file => {
-    const eventName = file.split('.')[0];
-    logger.log(`Loading Event: ${eventName}`);
-    const event = require(`./events/${file}`);
-    client.on(eventName, event.bind(null, client));
+    try {
+      const eventName = file.split('.')[0];
+      logger.log(`Loading Event: ${eventName}`);
+      const event = require(`./events/${file}`);
+      client.on(eventName, event.bind(null, client));
+    } catch (err) {
+      logger.error(`Failed to load event ${file}: ${err}`);
+    }
   });
 
   // Ensure database connection
   try {
     await sequelize.authenticate();
-    logger.ready('Connection to the database has been established successfully.');
+    logger.log('Connection to the database has been established successfully.');
   } catch (error) {
     logger.error(['Unable to connect to the database:', error]);
     process.exit(1);
   }
 
   // Login the client after connection to db
-  sequelize.sync({ force: false }).then(() => {
-    client.login(process.env.BOT_TOKEN,);
+  sequelize.sync({ force: false }).then(async () => {
+    await client.login(process.env.BOT_TOKEN);
+    // Put slash command routes
+    const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
+    try {
+      logger.log('Putting global slash command routes');
+      await rest.put(
+        Routes.applicationCommands(client.application.id),
+        { body: commands.values() },
+      );
+      logger.log('Global slash command routes put successfully')
+    } catch (err) {
+      logger.error(`Error putting slash command routes: ${err.stack}`);
+    }
   });
 }
 
---
+++ slash/bet.js
@@ -0,0 +1,94 @@
+const { isAdmin, pluralize } = require('../utils/helpers');
+const logger = require('../utils/logger');
+const { EmbedBuilder } = require('discord.js');
+
+exports.run = async (client, interaction) => {
+  logger.log(interaction);
+}
+
+exports.cmd = {
+  "name": "bet",
+  "description": "Create/end a bet or participate in an active bet",
+  "dm_permission": false,
+  "options": [
+    {
+      "type": 1,
+      "name": "cancel",
+      "description": "Cancel the active bet",
+      "options": []
+    },
+    {
+      "type": 1,
+      "name": "end",
+      "description": "End the bet with a winner",
+      "options": [
+        {
+          "type": 4,
+          "name": "winner",
+          "description": "Number of the winning bet option",
+          "choices": [
+            {
+              "name": "option 1",
+              "value": 1
+            },
+            {
+              "name": "option 1",
+              "value": 1
+            }
+          ],
+          "required": true
+        }
+      ]
+    },
+    {
+      "type": 1,
+      "name": "create",
+      "description": "Create a new bet with a title and two options",
+      "options": [
+        {
+          "type": 3,
+          "name": "option 2",
+          "description": "Second bet option",
+          "required": true
+        },
+        {
+          "type": 3,
+          "name": "option 1",
+          "description": "First bet option",
+          "required": true,
+          "choices": []
+        },
+        {
+          "type": 3,
+          "name": "title",
+          "description": "Title of the bet",
+          "required": true,
+          "choices": []
+        }
+      ]
+    },
+    {
+      "type": 1,
+      "name": "on",
+      "description": "Place a bet on the active bet",
+      "options": [
+        {
+          "type": 4,
+          "name": "option",
+          "description": "Option to bet on",
+          "required": true,
+          "choices": [
+            {
+              "name": "option 1",
+              "value": 1
+            },
+            {
+              "name": "option 2",
+              "value": 2
+            }
+          ]
+        }
+      ]
+    }
+  ],
+}
