3b0e7b9aa09f7a1a70230c08a0f79cbfec4d57f2
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-13 21:02:43 -0400

    Fix bugs

--- commands/bet.js
+++ commands/bet.js
@@ -1,41 +1,40 @@
 const { isAdmin, pluralize } = require('../utils/helpers');
 const logger = require('../utils/logger');
 const { EmbedBuilder } = require('discord.js');
-const getBets = () => new Map(Object.entries(JSON.parse(process.env.BETS)));
-const setBets = (bets) => process.env.BETS = JSON.stringify(Object.fromEntries(bets));
 
 exports.run = async (client, message, args) => {
   // Initialize and descture variables
-  const bets = getBets();
   const betId = message.guild.id;
-  let bet = bets.get(betId);
+  let bet = global.bets.get(betId);
   const { prefix, embedColor } = message.settings;
+  const [action, ...ops] = args;
 
-  // TODO: Fix the bets not saving properly
-  logger.log(bets);
   try {
+    // Check that the bet is active and open for betting
     const addBetPrecheck = () => {
-      // Check that the bet is active and open for betting
       if (!bet?.isActive()) throw new Error('There is no active bet, create a new one to start betting');
       if (!bet.isOpen()) throw new Error('Betting has been closed');
     }
 
-    switch (message.flags[0]) {
-      // Create cases for all flag aliases
+    // React to message to show operation was successful
+    const betSuccess = () => {
+      message.react('✅');
+    }
+
+    // Create cases for all bet command actrions and aliases
+    switch (action) {
       case 'create':
       case 'start':
       case 'new':
         // Check for active bet
         if (bet && bet.isActive()) throw new Error('The active bet must be ended before creating a new one')
         // Destructure bet creation operands
-        const name = args.shift();
-        const [one, two] = args.join(' ').split(',');
+        const [name, one, two] = ops.join(' ').split(',');
         // Check for valid bet creation criteria
         if (!(name && one && two)) throw new Error(`Invalid command usage, use \`${prefix}help bet\` for more information`);
         // Create new bet object
         const disp = await message.channel.send({ embeds: [new EmbedBuilder().setColor(embedColor).setTitle('Creating bet...')] });
         bet = new Bet(name, one, two, message.author, disp, embedColor)
-        bets.set(betId, bet);
         break;
       case 'cancel':
         bet.endBet();
@@ -44,30 +43,32 @@ exports.run = async (client, message, args) => {
       case 'finish':
         // Check for active bet
         if (!bet?.isActive()) throw new Error('There is no active bet to end');
-        const winner = typeof args[0] === 'string' ? parseInt(args[0]) : null;
+        const winner = typeof ops[0] === 'string' ? parseInt(ops[0]) : null;
+        logger.log(winner)
         if (!winner || (winner !== 1 && winner !== 2)) throw new Error('A valid winner must be given (1 or 2)');
         bet.endBet(winner);
         break;
       case 'one':
       case '1':
         addBetPrecheck();
-        bet.addBet(1, message.author.id)
+        bet.addBet(1, message.author.id);
+        betSuccess();
         break;
       case 'two':
       case '2':
         addBetPrecheck();
         bet.addBet(2, message.author.id);
+        betSuccess();
         break;
       default:
         throw new Error('No action specified');
     }
   } catch (err) {
-    logger.warn(err.stack);
+    logger.warn(err.message);
     message.reply(`❌ ${err.message}`);
   }
 
-  logger.log(bets);
-  setBets(bets);
+  global.bets.set(betId, bet)
 };
 
 exports.config = {
@@ -81,120 +82,128 @@ exports.config = {
 };
 
 class Bet {
-  #name;
-  #optionOne;
-  #optionTwo;
-  #open;
-  #active;
-  #winner;
-  #betOwner;
-  #display;
-  #embedColor;
+  name;
+  optionOne;
+  optionTwo;
+  open;
+  active;
+  winner;
+  betOwner;
+  display;
+  embedColor;
 
   constructor(name, optionOne, optionTwo, member, msg, color, closeDelay = 120000) {
-    this.#name = name;
-    this.#optionOne = {
+    this.name = name;
+    this.optionOne = {
       name: optionOne,
       betters: []
     };
-    this.#optionTwo = {
+    this.optionTwo = {
       name: optionTwo,
       betters: []
     };
-    this.#open = true;
-    this.#active = true;
-    this.#winner = null;
-    this.#betOwner = member;
-    this.#display = msg;
-    this.#embedColor = color;
-    this.#updateEmbed();
+    this.open = true;
+    this.active = true;
+    this.winner = null;
+    this.betOwner = member;
+    this.display = msg;
+    this.embedColor = color;
+    this.updateEmbed();
     setTimeout(() => {
-      this.#open = false;
-      this.#updateEmbed();
+      this.closeBet();
+      this.updateEmbed();
+      this.display.channel.send({ embeds: [new EmbedBuilder().setTitle(this.name).setDescription('Betting is now closed')] });
     }, closeDelay);
   }
 
-  #getBetters(option) {
-    return option === 1 ? this.#optionOne.betters : this.#optionTwo.betters
+  getBetters(option) {
+    return option === 1 ? this.optionOne.betters : this.optionTwo.betters
   }
 
-  #getEmbed() {
+  getEmbed() {
     const [activeStatus, bettingStatus] = [
-      this.#active ? 'Active' : 'Inactive',
-      this.#open ? 'Open' : 'Closed'
+      this.active ? 'Active' : 'Finished',
+      this.open ? 'Open' : 'Closed'
     ]
     return new EmbedBuilder()
-      .setColor(this.#embedColor)
-      .setTitle(this.#name)
+      .setColor(this.embedColor)
+      .setTitle(this.name)
       .setDescription(`Status: ${activeStatus} | Betting: ${bettingStatus}`)
       .addFields(
         {
-          name: `1️⃣ ${this.#optionOne.name}`,
-          value: `${this.#getBetters(1).length} better${pluralize(this.#getBetters(1).length)}`
+          name: `1️⃣ ${this.optionOne.name}`,
+          value: `${this.getBetters(1).length} better${pluralize(this.getBetters(1).length)}`
         },
         {
-          name: `2️⃣ ${this.#optionTwo.name}`,
-          value: `${this.#getBetters(2).length} better${pluralize(this.#getBetters(2).length)}`
+          name: `2️⃣ ${this.optionTwo.name}`,
+          value: `${this.getBetters(2).length} better${pluralize(this.getBetters(2).length)}`
         }
       )
   }
 
-  #getResultEmbed(option) {
-    const winBetters = this.#getBetters(option).length;
+  getResultEmbed(option) {
+    const winBetters = this.getBetters(option).length;
     return new EmbedBuilder()
-      .setColor(this.#embedColor)
-      .setTitle(this.#name)
-      .setDescription(`Winner: ${this.#winner} with ${winBetters} better${pluralize(pluralize(winBetters))}`)
+      .setColor(this.embedColor)
+      .setTitle(this.name)
+      .setDescription(`Winner: ${this.winner} with ${winBetters} better${pluralize(pluralize(winBetters))}`)
   }
 
-  #updateEmbed() {
-    this.#display.edit({ embeds: [this.#getEmbed()] });
+  updateEmbed() {
+    this.display.edit({ embeds: [this.getEmbed()] });
   }
 
   isBetAdmin(member) {
-    return (this.#betOwner.id === member.id || isAdmin(member));
+    return (this.betOwner.id === member.id || isAdmin(member));
   }
 
   isActive() {
-    return this.#active;
+    return this.active;
   }
 
   isOpen() {
-    return this.#open;
+    return this.open;
+  }
+
+  closeBet() {
+    this.open = false;
   }
 
   addBet(option, memberId) {
-    if (this.#getBetters(1).contains(memberId) || this.#getBetters(2).contains(memberId))
+    if (this.getBetters(1).includes(memberId) || this.getBetters(2).includes(memberId))
       throw new Error('You may only bet once');
     switch (option) {
       case undefined: throw new Error('No bet option provided');
       case 1:
-        this.#optionOne.betters.push(memberId);
+        this.optionOne.betters.push(memberId);
         break;
       case 2:
-        this.#optionTwo.betters.push(memberId);
+        this.optionTwo.betters.push(memberId);
         break;
       default: throw new Error('Invalid bet option provided');
     }
-    this.#updateEmbed();
+    this.updateEmbed();
   }
 
   endBet(winner) {
     switch (winner) {
       case undefined:
-        this.#active = false;
-        this.#updateEmbed();
+        this.closeBet();
+        this.active = false;
+        this.updateEmbed();
         return true;
       case 1:
-        this.#active = false;
-        this.#winner = this.#optionOne.name;
-        this.#updateEmbed();
-        return [this.#getResultEmbed(1), this.#getBetters(1), this.#getBetters(2)];
+        this.closeBet();
+        this.active = false;
+        this.winner = this.optionOne.name;
+        this.updateEmbed();
+        return [this.getResultEmbed(1), this.getBetters(1), this.getBetters(2)];
       case 2:
-        this.#active = false;
-        this.#winner = this.#optionTwo.name;
-        this.#updateEmbed();
-        return [this.#getResultEmbed(2), this.#getBetters(2), this.#getBetters(1)];
+        this.closeBet();
+        this.active = false;
+        this.winner = this.optionTwo.name;
+        this.updateEmbed();
+        return [this.getResultEmbed(2), this.getBetters(2), this.getBetters(1)];
       default:
         return false;
     }
