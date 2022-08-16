2cef0740981a0f9b239faaa89560638d1872578b
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-16 19:57:06 -0400

    Update relationships

--- db/models/GuildMember.js
+++ db/models/GuildMember.js
@@ -2,12 +2,6 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../connection');
 
 const GuildMember = sequelize.define('GuildMember', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false
-  },
   betWins: {
     type: DataTypes.INTEGER,
     defaultValue: 0,
--- db/models/index.js
+++ db/models/index.js
@@ -2,7 +2,7 @@ const Guild = require('./Guild');
 const User = require('./User');
 const GuildMember = require('./GuildMember');
 
-Guild.belongsToMany(User, { through: GuildMember });
+Guild.hasMany(User, { through: GuildMember });
 User.belongsToMany(Guild, { through: GuildMember });
 
 module.exports = { Guild, User, GuildMember }
