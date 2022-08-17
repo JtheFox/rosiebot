63896605df6de74e882efbf0fa2e056fbc72ab47
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-17 18:13:55 -0400

    Set user presence on start

--- events/ready.js
+++ events/ready.js
@@ -19,5 +19,5 @@ module.exports = async client => {
   }
 
   logger.ready(`${client.user.tag}, ready to serve ${client.guilds.cache.map(g => g.memberCount).reduce((a, b) => a + b)} users in ${client.guilds.cache.size} servers.`);
-  client.user.setActivity(`${defaultSettings.prefix}`, { type: 'PLAYING' });
+  client.user.setPresence({ activities: [{ name: `${defaultSettings.prefix} help` }], type: 'PLAYING' });
 };
