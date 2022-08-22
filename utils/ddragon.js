19b2e5b4b081b83f35727fbdf041f9e693c47bb1
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-22 14:43:25 -0400

    Add get all items function

--- utils/ddragon.js
+++ utils/ddragon.js
@@ -9,5 +9,14 @@ module.exports = {
   getAllChampions: async () => {
     const res = await axios.get(`${domain}/cdn/${global.ddragVersion}/data/en_US/champion.json`);
     return Object.values(res.data.data);
-  }
+  },
+  getAllItems: async () => {
+    const res = await axios.get(`${domain}/cdn/${global.ddragVersion}/data/en_US/item.json`);
+    const allItems = Object.values(res.data.data);
+    return {
+      boots: allItems.filter(({ tags, depth }) => tags.includes('Boots') && depth === 2),
+      mythic: allItems.filter(({ description }) => description.includes('Mythic')),
+      legendary: allItems.filter(({ description, depth }) => !description.includes('Mythic') && depth === 3)
+    }
+  },
 }
