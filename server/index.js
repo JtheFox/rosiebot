a3fd7d59b2cea3bc47a894f5a2f311fff9ab3ca1
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-08-29 16:57:49 -0400

    Create separate web process for server

--- server/index.js
+++ server/index.js
@@ -2,30 +2,27 @@ const logger = require('../utils/logger');
 const axios = require('axios');
 const app = require('express')();
 const PORT = process.env.PORT || 3000;
+const token = process.env.BOT_TOKEN;
 
-exports.start = async (token) => {
-  if (!token) throw new Error('No token provided');
+const discordApiConfig = { headers: { Authorization: `Bot ${token}` } };
+const discordApiUrl = 'https://discord.com/api/v9';
 
-  const discordApiConfig = { headers: { Authorization: `Bot ${token}` } };
-  const discordApiUrl = 'https://discord.com/api/v9';
+app.get('/', (req, res) => {
+  res.redirect('https://github.com/JtheFox/rosiebot');
+});
 
-  app.get('/', (req, res) => {
-    res.redirect('https://github.com/JtheFox/rosiebot');
+app.get('/stats', async (req, res) => {
+  let botUsers = 0;
+  const botGuilds = await axios.get(`${discordApiUrl}/users/@me/guilds`, discordApiConfig);
+  for await (let guild of botGuilds.data) {
+    const guildRes = await axios.get(`${discordApiUrl}/guilds/${guild.id}/preview`, discordApiConfig);
+    botUsers += guildRes.data.approximate_member_count;
+  }
+
+  res.status(200).json({
+    users: botUsers,
+    servers: botGuilds.data.length
   });
+});
 
-  app.get('/stats', async (req, res) => {
-    let botUsers = 0;
-    const botGuilds = await axios.get(`${discordApiUrl}/users/@me/guilds`, discordApiConfig);
-    for await (let guild of botGuilds.data) {
-      const guildRes = await axios.get(`${discordApiUrl}/guilds/${guild.id}/preview`, discordApiConfig);
-      botUsers += guildRes.data.approximate_member_count;
-    }
-
-    res.status(200).json({
-      users: botUsers,
-      servers: botGuilds.data.length
-    });
-  });
-
-  app.listen(PORT, () => logger.ready('Sever is now listening'));
-}
+app.listen(PORT, () => logger.ready('Sever is now listening'));
