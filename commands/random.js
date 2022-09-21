96dcf86e0a99a101419aa1c0434d975380377ee0
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-09-21 13:21:40 -0400

    Fix random chat command

--- commands/random.js
+++ commands/random.js
@@ -4,12 +4,13 @@ const { EmbedBuilder } = require('discord.js');
 
 exports.run = async (client, message, args) => {
   const champList = await getAllChampions();
-  const randChamp = arrayRandom(champList);
+  const { name, alias, id } = arrayRandom(champList);
   const champEmbed = new EmbedBuilder()
     .setColor(message.settings.embedColor)
-    .setTitle(randChamp.name)
-    .setDescription(randChamp.title.charAt(0).toUpperCase() + randChamp.title.slice(1))
-    .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/${randChamp.version}/img/champion/${randChamp.id}.png`);
+    .setAuthor({ name: 'View on U.GG', url: `https://u.gg/lol/champions/${alias}/build` })
+    .setTitle(name)
+    // .setDescription(title.charAt(0).toUpperCase() + title.slice(1))
+    .setThumbnail('https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/' + id + '.png')
   message.channel.send({ embeds: [champEmbed] })
 };
 
