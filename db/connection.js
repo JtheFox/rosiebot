6d7b2c95fd53b514e41ca93408135bd46d7b349b
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-10-15 13:03:31 -0400

    Add procfile & update db config

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_ENV === 'local' ?
-  new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  })
-  : new Sequelize(
-    process.env.RDS_NAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD,
-    {
-      host: process.env.RDS_HOST,
-      dialect: 'mysql',
-      port: process.env.RDS_PORT,
-      logging: false
-    }
-  );
+const sequelize = new Sequelize(process.env.JAWSDB_URL)
 
 module.exports = sequelize;
